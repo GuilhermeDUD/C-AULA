16b9f87b8417df5e7739770ee29fdd15efd7b449
GuilhermeDUD <guidoudement@gmail.com>
2024-08-20 20:48:48 -0300

    bloco de notas

---
+++ Aula-20-08-C/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int 1 = 1;
+    printf("%d Hello World 2024 \n", i++);
+    printf("%d Hello World 2024 \n", i++);
+    printf("%d Hello World 2024 \n", i++);
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+git config user.name "GuilhermeDUD"
+git config user.email "guidoudement@gmail.com"
+git add .
+git commit -m "mensagem"
+git push origin main
+
