9fff187a3f696f389322e9c1335512a6d220c2d1
Guilherme Doudement <163214700+GuilhermeDUD@users.noreply.github.com>
2024-10-07 21:01:59 -0300

    Add files via upload

---
+++ Aula-16-09-C/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define QUANTIDADE (40)
+    // inicialize um vetor com os numeros aleatorios
+    // busque nesse valor os elementos vizinhos com maior diferenca
+
+int main (){
+    int aleatorio(QUANTIDADE);
+
+    //gerar numeros aleatorios
+    srand(time(NULL));
+
+    printf("Vetor:");
+    for (i = 0; i < QUANTIDADE; i++)
+    {
+        aleatorio[1] = rand;
+        printf("%d",&aleatorio[1]);
+    }
+    printf("\n")
+
+    for (i = 0; i < QUANTIDADE; i++)
+    {
+        /* code */
+    }
+    
+
+
+
+
+}
